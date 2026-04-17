b89a713e949817407dc2a21e3db78e347ae34c17
klassrun <klassrun@gmail.com>
2026-04-17 13:20:17 +0100

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,12 +1,11 @@
 export const dynamic = 'force-static'
+
 export default function robots() {
   return {
     rules: [
       {
         userAgent: '*',
         allow: '/',
-        // disallow any private routes when you build them e.g:
-        // disallow: ['/dashboard/', '/admin/'],
       },
     ],
     sitemap: 'https://klassrun.com/sitemap.xml',
